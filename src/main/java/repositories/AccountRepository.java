c24a276a1e222c49073f2220ef576f21ad0dd1c6
Antoine FORET <antoine.foret@epitech.eu>
2018-12-09 15:13:09 +0200

    [+] Adding comment on repositories

--- src/main/java/repositories/AccountRepository.java
+++ src/main/java/repositories/AccountRepository.java
@@ -3,7 +3,12 @@ package repositories;
 import annotations.Repository;
 import models.Account;
 
+/**
+ * Account repository. Used to retrieve accounts from database and create java instances
+ * @see models.Account
+ * @author Antoine FORET
+ * @version 1.0
+ */
 @Repository(model = Account.class)
 public class AccountRepository extends RepositoryBase {
-
 }
--- src/main/java/repositories/TransactionRepository.java
+++ src/main/java/repositories/TransactionRepository.java
@@ -3,6 +3,12 @@ package repositories;
 import annotations.Repository;
 import models.Transaction;
 
+/**
+ * Transaction repository. Used to retrieve transactions from database and create java instances
+ * @see models.Account
+ * @author Antoine FORET
+ * @version 1.0
+ */
 @Repository(model = Transaction.class)
-public class TransactionRepository extends RepositoryBase{
+public class TransactionRepository extends RepositoryBase {
 }
