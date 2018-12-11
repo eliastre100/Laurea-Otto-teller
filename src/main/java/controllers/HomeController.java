e973095cc40d5bf53f80d3455adc50ce3b598a92
Antoine FORET <antoine.foret@epitech.eu>
2018-12-11 16:20:46 +0200

    [+] Adding Controllers commentaries

--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -10,12 +10,37 @@ import views.HomeView;
 import java.awt.event.ActionListener;
 import java.util.*;
 
+/**
+ * Main controller of the application. This class handle all the logic from actions of an authenticated user.
+ * @author Antoine FORET
+ * @version 1.0
+ */
 public class HomeController {
-    HomeView view;
-    Account account;
-    AccountRepository accountRepository;
-    TransactionRepository transactionRepository;
 
+    /**
+     * The home view (swing components goes there)
+     */
+    private HomeView view;
+
+    /**
+     * The current account we manage. This property is defined by the constructor and as such is a mandatory parameter for this controller. It is also updated every time we use it.
+     */
+    private Account account;
+
+    /**
+     * Repository to retrieve accounts. It is used to update the account parameter.
+     */
+    private AccountRepository accountRepository;
+
+    /**
+     * Repository to retrieve the account's transactions (the relationship is unidirectional)
+     */
+    private TransactionRepository transactionRepository;
+
+    /**
+     * Constructor of the controller. It need the account it will manage, then it update the view and all it"s properties
+     * @param account the account to manage
+     */
     HomeController(Account account) {
         HashMap<String, ActionListener> handlers = new HashMap<>();
         handlers.put("summary", e -> this.summary());
@@ -31,11 +56,18 @@ public class HomeController {
         this.view = new HomeView(account, handlers);
     }
 
+    /**
+     * Ask the view to display the account summary
+     */
     private void summary() {
         this.updateAccount();
         this.view.showSummary(this.account);
     }
 
+    /**
+     * Open the page with the summary of all the user's transactions.
+     * We need to remove some duplicate entries as a withdraw is a transaction that have the account as both initiator and recipient.
+     */
     private void transactions() {
         this.updateAccount();
         List<Transaction> transactions = this.transactionRepository.findBy("initiator_id", this.account);
@@ -51,6 +83,9 @@ public class HomeController {
         new TransactionController(this.account, transactions);
     }
 
+    /**
+     * Ask the view the different data need to create a new withdraw, then validate the datas and create the withdraw transaction while updating the account balance.
+     */
     private void withdraw() {
         this.updateAccount();
 
@@ -70,6 +105,9 @@ public class HomeController {
         }
     }
 
+    /**
+     * Ask the view the different parameters from the user to create a new transfer, then validate the datas and create the transfer in base while updating the concerned accounts
+     */
     private void transfer() {
         try {
             this.updateAccount();
@@ -94,6 +132,9 @@ public class HomeController {
         }
     }
 
+    /**
+     * Delete the user account. This action is not reversible and will log the user off.
+     */
     private void delete() {
         System.out.println("[INFO] User " + this.account.getIdentifier() + " just deleted it's account!");
         this.account.destroy();
@@ -101,12 +142,18 @@ public class HomeController {
         this.view.dispose();
     }
 
+    /**
+     * Log out the user by closing the windows as no data are persisted else for the session
+     */
     private void logout() {
         System.out.println("[INFO] User " + this.account.getIdentifier() + " just logged out");
         this.view.setVisible(false);
         this.view.dispose();
     }
 
+    /**
+     * Helper to update the account property and keep it up to date.
+     */
     private void updateAccount() {
         this.account = this.accountRepository.find(this.account.getId());
     }
--- src/main/java/controllers/TransactionController.java
+++ src/main/java/controllers/TransactionController.java
@@ -6,14 +6,24 @@ import views.TransactionsView;
 
 import java.util.List;
 
+/**
+ * Transaction controller. Used to display the transaction summary to the user. It doesn't contain any logic but to keep our code clean, we split the logic and display parts anyway.
+ * @author Antoine FORET
+ * @version 1.0
+ */
 public class TransactionController {
-    Account account;
-    List<Transaction> transactions;
+
+    /**
+     * The actual view
+     */
     TransactionsView view;
 
+    /**
+     * Initiate the view to display all the transactions.
+     * @param account the account we display the transactions
+     * @param transactions the transaction list from the user.
+     */
     public TransactionController(Account account, List<Transaction> transactions) {
-        this.account = account;
-        this.transactions = transactions;
         this.view = new TransactionsView(account, transactions);
     }
 }
